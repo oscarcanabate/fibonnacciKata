7e800f112e3f9a17fd71ba8bd38f7c5b99d06f5a
OscarCañabateMarín <oscar.canabate@grupotrebolenergia.es>
2022-02-25 14:27:42 +0100

    feat: test_position_1_returns_1

---
+++ fibonacci_should.py
@@ -0,0 +1,31 @@
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+
+import unittest
+
+from Fibonacci import Fibbonacci
+
+
+class Fibonacci_should(unittest.TestCase):
+    def test_number_0_returns_1(self):
+        #Arrange
+        number = 0
+
+        #Act
+        result = Fibbonacci().act(number)
+
+        #Assert
+        self.assertEqual(result,1)
+
+    def test_position_1_returns_1(self):
+        #Arrange
+        number = 1
+
+        #Act
+        result = Fibbonacci().act(number)
+
+        #Assert
+        self.assertEqual(result,1)
