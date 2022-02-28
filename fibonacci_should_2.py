b04c1ea9e34b89d0a64845f6bce9fe949307c2bc
OscarCañabateMarín <oscar.canabate@grupotrebolenergia.es>
2022-02-28 17:11:58 +0100

    refactor: tests con pytest

---
+++ fibonacci_should_2.py
@@ -0,0 +1,34 @@
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR#####OSCAR
+
+##Como usar pytest en pycharm:
+# -> ctrl+alt+s
+# -> escribir pytest en el buscador
+# -> cambiar el framework de testing por defecto a pytest
+# -> escribir un test
+
+# Ejemplo de tests
+from Fibonacci import Fibbonacci
+
+def test_position_0_returns_1():
+    #Arrange
+    number = 0
+
+    #Act
+    result = Fibbonacci().act(number)
+
+    #Assert
+    assert result == 1
+
+def test_position_1_returns_1():
+    #Arrange
+    number = 1
+
+    #Act
+    result = Fibbonacci().act(number)
+
+    #Assert
+    assert result == 1
